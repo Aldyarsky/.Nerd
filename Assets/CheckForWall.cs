d38a9e6361e39c6bc72ce8110d0f534022f6bfa4
Aldyar Malyk <aldyar71@gmail.com>
2020-11-24 14:06:06 +0600

    first commit

---
+++ Assets/CheckForWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForWall : MonoBehaviour
+{
+    private bool isWallNear = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            isWallNear = true;
+        }
+        print(isWallNear);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            isWallNear = false;
+        }
+        print(isWallNear);
+    }
+}
